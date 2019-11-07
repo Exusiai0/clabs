45483c18596bad40129a727a3b476c7ee4f7d8af
Exusiai0 <1604316989@qq.com>
2019-11-07 05:32:50 -0800

    added a ex1.c file

--- ex1.c
+++ ex1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int max_int = (1<<31) - 1;
-    printf("The max value of int on 32 machine:%d\n", max_int);
-    unsigned int max_uint = 0 - 1;
-    printf("The max value of unsigned int on 32 machine:%u\n",max_uint);
+    unsigned int x=1;
+    unsigned int y=0-1;
+    unsigned int z=0;
+    while(x!=0){
+        x=x<<1;
+        z=z+1;
+    }
+    printf("x=%d\n",z);
+    printf("%u\n",y);
+    return 0;
 }
---
+++ ex2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdio.h>
+int main(void){
+    int x;
+    int y=0;
+    int z[100];
+    int count=0;
+    printf("/*s")
+}
