8041f9f15af8227f942bb1044afc50f41722d2dc
Exusiai0 <1604316989@qq.com>
2019-12-26 04:42:04 -0800

    add a l7.c

--- lab7/l7.c
+++ lab7/l7.c
@@ -1,26 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-int main(int argc,char*argv[])
+int main(int argc, char *argv[])
 {
-    int s[100];
-    int a =1;
-    int b = 0;
-    int x=0;
-    int y = 0;
-    int temp;
-    for(a = 1;a < argc;a++,b++)
-    {
-        s[b] = atof(argv[a]);
-    }
-    for(x = 0;x < argc-2;x++)
-    {
-     if(s[x] < s[x+1]) 
+int a[100];
+int i=1;
+int x=0;
+for(i=1;i<argc;i++,x++)
 {
-temp=s[x];
-s[x]=s[x+1];
-s[x+1]=temp;
+     a[x]=atof(argv[i]);
 }
-}
-printf("The min is：%d\n",s[argc-2]);
-return 0;
+    int i ;
+    int min=a[0];
+     for(i=0;i<argc;i++)
+     {
+if(a[i]<min){
+min=a[i];
+ }
+     }
+printf("%d\n",min);
 }
